8c46c630672e90f7dfc43a93679348b95cc7076c
moznion <moznion@gmail.com>
2014-11-08 12:53:43 +0900

    Setup project base

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+
+gulp.task('test', function () {
+    if (typeof process.env.NODE_ENV === 'undefined') {
+        process.env.NODE_ENV = 'test';
+    }
+    return gulp.src('test/*', {read: false})
+        .pipe(mocha({ui: 'bdd', reporter: 'spec'}));
+});
+
+gulp.task('lint', function () {
+    return gulp.src(['src/js/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
+});
