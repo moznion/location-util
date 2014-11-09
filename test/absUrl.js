ea22ee1596e2f9d93ce01349f06a3c99143f21bb
moznion <moznion@gmail.com>
2014-11-09 22:40:10 +0900

    Add some test cases that are in doc

--- test/absUrl.js
+++ test/absUrl.js
@@ -8,5 +8,13 @@ describe('#absUrl', function () {
         var l = new LocationUtil('http://example.com:8080/foo?bar=buz#frag');
         l.absUrl().should.equal('http://example.com:8080/foo?bar=buz#frag');
     });
+
+    it('documents behavior', function () {
+        var l = new LocationUtil('http://example.com:3000/foo?bar=buz#frag');
+        l.url('/user?id=123#name').absUrl().should.equal('http://example.com:3000/user?id=123#name');
+        l.path('/entry').absUrl().should.equal('http://example.com:3000/entry?id=123#name');
+        l.search('date', '20140401', 'id', null).absUrl().should.equal('http://example.com:3000/entry?date=20140401#name');
+        l.hash('').absUrl().should.equal('http://example.com:3000/entry?date=20140401');
+    });
 });
 
